27508cee131db37abf1076e7ee87e3ea46fba45f
Mark <hello@markgbrown.design>
2025-05-11 12:56:23 +0100

    Initial Commit

---
+++ bloom/bloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  bloomApp.swift
+//  bloom
+//
+//  Created by Mark Brown on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct bloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bloomTests/bloomTests.swift
@@ -0,0 +1,16 @@
+//
+//  bloomTests.swift
+//  bloomTests
+//
+//  Created by Mark Brown on 11/05/2025.
+//
+
+import Testing
+
+struct bloomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
